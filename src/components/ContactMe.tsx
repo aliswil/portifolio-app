05e7d20983618b5fd8bf19eb3ad9a6de6554fd16
Alis Wiken Wilson <aliskwn@gmail.com>
2022-10-30 23:29:26 +0100

    changed jsx to tsx

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const ContactMe = () => {
   return (
-    <div name="contact" className="w-full h-screen bg-[#edede9] text-gray-700">
+    <div
+      data-name="contact"
+      className="w-full h-screen bg-[#edede9] text-gray-700"
+    >
       <div className="flex flex-col justify-center items-center w-full h-full">
         <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
           <div className="sm:text-right pb-8 pl-4">
