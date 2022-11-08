4c4a00ed4ebd0a5891b9080b45f37226e44d9373
Alis Wiken Wilson <aliskwn@gmail.com>
2022-11-08 19:36:30 +0100

    changed files from js to tsx

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -14,10 +14,10 @@ const AboutMe = () => {
         </div>
         <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
           <div className="sm:text-right text-3xl ">
-            <p>Hi. I´m Alis, nice to meet you. Pleace take a look around.</p>
+            <p>Hi. I´m Ada, nice to meet you. Pleace take a look around.</p>
           </div>
           <div>
-            <p>I am passionate about tech, blablablablablabal</p>
+            <p>I am passionate about tech and solving problems with code.</p>
           </div>
         </div>
       </div>
