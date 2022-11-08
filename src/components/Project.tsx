013254a7eecdfb58cc770fd51b8b3f4fd4268f9d
Alis Wiken Wilson <aliskwn@gmail.com>
2022-11-08 13:19:20 +0100

    added project site

---
+++ src/components/Project.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import Project1 from "../assets/wiken_design.png";
+
+const Project = () => {
+  return (
+    <div className="w-screen flex sm:flex-row flex-col justify-evenly">
+      <div className="max-w-m rounded overflow-hidden shadow-lg bg-[#edede9]">
+        <img className="w-full" src={Project1} alt="Sunset in the mountains" />
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2 text-black">Wiken Design</div>
+          <p className="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #photography
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #travel
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #winter
+          </span>
+        </div>
+      </div>
+      <div className="max-w-m rounded overflow-hidden shadow-lg bg-[#edede9]">
+        <img className="w-full" src={Project1} alt="Sunset in the mountains" />
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2 text-black">Wiken Design</div>
+          <p className="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #photography
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #travel
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #winter
+          </span>
+        </div>
+      </div>
+      <div className="max-w-m rounded overflow-hidden shadow-lg bg-[#edede9]">
+        <img className="w-full" src={Project1} alt="Sunset in the mountains" />
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2 text-black">Wiken Design</div>
+          <p className="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #photography
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #travel
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #winter
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Project;
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,15 +1,16 @@
+//import { listenerCount } from "process";
 import React from "react";
+import Project from "./Project";
 
 const Projects = () => {
   return (
     <div id="projects" className="w-full h-screen bg-[#22333b] text-white">
       <div className="flex flex-col justify-center items-center w-full h-full">
-        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
-          <div className="sm:text-right pb-8 pl-4">
-            <p className="text-4xl font-bold inline border-b-4 border-gray-700">
-              Projects
-            </p>
-          </div>
+        <p className="text-4xl font-bold inline border-b-4 border-gray-700">
+          Projects
+        </p>
+        <div className="pt-20">
+          <Project />
         </div>
       </div>
     </div>
