d9a87f329292e3a8c3daa300b34c058d4eb23133
Henriette Brekke Sunde <henriesu@stud.ntnu.no>
2022-11-09 19:54:57 +0100

    .

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -7,7 +7,7 @@ const AboutMe = () => {
         <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
           <div className="sm:text-right pb-8 pl-4">
             <p className="text-4xl font-bold inline border-b-4 border-[#335b20]">
-              About
+              About me.
             </p>
           </div>
           <div></div>
